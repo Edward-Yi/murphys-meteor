6f4a2b90fa84f43e42aab86c69dd2524be6d8671
Edward Yi <42788217+Edward-Yi@users.noreply.github.com>
2018-10-16 12:38:29 -1000

    Completed Murphys mockup

---
+++ app/imports/ui/pages/Murphys.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MurphysBackground from '../components/MurphysBackground';
+
+export default class Murphys extends React.Component {
+
+  render() {
+    return (
+        <div>
+          <MurphysBackground/>
+        </div>
+    );
+  }
+}
